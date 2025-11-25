dda785e0f5285886d4f8190d2519dbbb38629e83
Aaron <hellmulleraaron@gmail.com>
2025-11-25 15:38:25 +0100

    Rust Data Types

---
+++ w3-rust-tutorial/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    // Strings
+    let mut name: &str = "Aaron"; // mut to make the variable "mutable"(changeble)
+    println!("Hello, {}", name);
+    name = "Sigma";
+    println!("Hello, {}", name);
+
+    // Whole numbers
+    let age: i32 = 16;
+    println!("Age is: {}", age);
+
+    // Floating point numbers
+    let price: f64 = 69.69;
+    println!("Price is: {}", price);
+
+    // Characters
+    let myGrade: char = 'A';
+    println!("{}", myGrade);
+
+    // Booleans
+    let is_logged_in: bool = true;
+    println!("User logged in? {}", is_logged_in);
+}
