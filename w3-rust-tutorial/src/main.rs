117b961a9fa38f2ed11b34e1fe6358bef1b7ed49
Aaron <hellmulleraaron@gmail.com>
2026-01-10 10:42:52 +0100

    RUst Borrowing

--- w3-rust-tutorial/src/main.rs
+++ w3-rust-tutorial/src/main.rs
@@ -205,6 +205,19 @@ fn main() {
     let b = a.clone(); // Now both have the same value
     println!("a = {}", a); // Works
     println!("b = {}", b); // Works 
+
+    // Borrowing
+    // Reference (like a pointer)
+    let a = String::from("Hello");
+    let b = &a;
+    println!("a = {}", a);
+    println!("b = {}", b);
+
+    // Mutable Reference
+    let mut name = String::from("John");
+    let name_ref = &mut name;
+    name_ref.push_str(" Doe");
+    println!("{}", name_ref); // John Doe 
 }
 
 fn say_hello() {
