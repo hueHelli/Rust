e770c97ffe3fb6dd7d5156b3338b6804c3d3c454
Aaron <hellmulleraaron@gmail.com>
2026-01-10 11:15:26 +0100

    Rust Vectors

--- w3-rust-data-structures/src/main.rs
+++ w3-rust-data-structures/src/main.rs
@@ -16,4 +16,22 @@ fn main() {
     // When printing the whole array, you must use {:?} inside println!()
     let numbers = [1, 2, 3, 4, 5];
     println!("{:?}", numbers);
+    // You can also use len() on Arrays
+
+    // Vectors
+    // A vector is a reziable array, it has all the functions an array has as well. To create one use the vec! macro
+    let mut fruits = vec!["apple", "banana"];
+    fruits.push("cherry");
+    println!("{:?}", fruits); // ["apple", "banana", "cherry"]
+    fruits.pop();
+    println!("{:?}", fruits); // ["apple", "banana"]
+
+    // You can also insert values at specific indexes
+    let mut fruits = vec!["banana", "orange"];
+    fruits.insert(0, "apple");
+    println!("{:?}", fruits); // ["apple", "banana", "orange"]
+    fruits.remove(0); // The lower the index the slower it is, because a lot of items have to shift positions
+    println!("{:?}", fruits); // ["banana", "orange"]
+
+    // When you loop through a Vector you should use a reference, so you can still use it later
 }
