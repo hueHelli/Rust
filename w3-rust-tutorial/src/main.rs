e105de5e0c62bd12b08be6670b8b9a16fcf7a269
Aaron <hellmulleraaron@gmail.com>
2026-01-10 10:04:17 +0100

    Rust Match

--- w3-rust-tutorial/src/main.rs
+++ w3-rust-tutorial/src/main.rs
@@ -14,8 +14,8 @@ fn main() {
     println!("Price is: {}", price);
 
     // Characters
-    let myGrade: char = 'A';
-    println!("{}", myGrade);
+    let my_grade: char = 'A';
+    println!("{}", my_grade);
 
     // Booleans
     let is_logged_in: bool = true;
@@ -75,11 +75,46 @@ fn main() {
     println!("Not logged in: {}", !logged_in);
 
     // If / Else
-    let is_logged_in = true;
+    let is_logged_in: bool = true;
 
     if is_logged_in {
         println!("Welcome back!");
     } else {
         println!("Please log in.");
     }
+
+    let is_allowed: &str = if is_logged_in { "Yes" } else { "No" };
+    println!("{}", is_allowed);
+
+    let day: i32 = 4;
+
+    match day {
+        1 => println!("Monday"),
+        2 => println!("Tuesday"),
+        3 => println!("Wednesday"),
+        4 => println!("Thursday"),
+        5 => println!("Friday"),
+        6 => println!("Saturday"),
+        7 => println!("Sunday"),
+        _ => println!("Invalid day."),
+    }
+
+    match day {
+        1 | 2 | 3 | 4 | 5 => println!("Weekday"),
+        6 | 7 => println!("Weekend"),
+        _ => println!("Invalid day"),
+    }
+
+    let result = match day {
+        1 => "Monday",
+        2 => "Tuesday",
+        3 => "Wednesday",
+        4 => "Thursday",
+        5 => "Friday",
+        6 => "Saturday",
+        7 => "Sunday",
+        _ => "Invalid day.",
+    };
+
+    println!("{}", result);
 }
